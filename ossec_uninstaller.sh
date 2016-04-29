befd1afa5601541b8f6f8e3cd3c456bfd1131a40
root <mcsnet@yahoo.com>
2016-04-29 10:47:18 +1000

    Added tar function.  Tested and working.

--- ossec_uninstaller.sh
+++ ossec_uninstaller.sh
@@ -3,15 +3,21 @@
 # Tested in Cent 6.x and Ubuntu 16.04 #
 # Using OSSEC versions 2.7, 2.7.1, 2.8, 2.8.1, 2.8.2, 2.8.3 and 2.9.0beta5 #
 
+tarloc=/tmp/ossec-`date +"%m-%d-%y-%T"`.tgz
+
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
- 
+
+echo "Tarring up the OSSEC files before we continue..."
+tar zcf ${tarloc} /var/ossec /etc/init.d/*ossec* 
+echo "### Your backup is in $tarloc ###"
+
 echo "Stopping the OSSEC daemon..."
 if [ -f /var/ossec/bin/ossec-control ];
         then /var/ossec/bin/ossec-control stop;
         else echo "Can't find ossec-control.  Trying ossec service.."
         service ossec stop || service ossec-hids stop
 fi
- 
+
 echo "Removing /var/ossec directory..."
 rm -rf /var/ossec
 echo "Removing OSSEC init files..."
@@ -20,3 +26,6 @@ echo "Removing OSSEC users..."
 for i in `grep ossec /etc/passwd | gawk -F: '{print $1}'`; do /usr/sbin/userdel -r $i;done
 echo "Removing OSSEC group..."
 /usr/sbin/groupdel ossec
+
+
+
