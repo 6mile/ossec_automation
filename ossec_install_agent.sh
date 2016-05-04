7bc9d3c8c4d4264e63de8c720d05f72b08e484bd
root <mcsnet@yahoo.com>
2016-05-04 16:31:24 +1000

    added ubuntu for both install and uninstall as well as the logic to choose

--- ossec_install_agent.sh
+++ ossec_install_agent.sh
@@ -3,8 +3,21 @@
 serverip=192.168.0.254 # Put your own OSSEC server's IP address here.
 osseccf=/var/ossec/etc/ossec.conf
 
+redhat () {
 yum install -y http://www5.atomicorp.com/channels/ossec/centos/6/x86_64/RPMS/ossec-hids-2.8.3-53.el6.art.x86_64.rpm
 yum install -y http://www5.atomicorp.com/channels/ossec/centos/6/x86_64/RPMS/ossec-hids-client-2.8.3-53.el6.art.x86_64.rpm
+}
+
+debian () {
+wget http://ossec.wazuh.com/repos/apt/ubuntu/pool/main/o/ossec-hids-agent/ossec-hids-agent_2.8.3-3wily_amd64.deb -O /tmp/debian_ossec_agent.deb
+apt-get install -y /tmp/debian_ossec_agent.deb
+}
+
+if [ -f /etc/redhat-release ]; 
+	then echo "REDHAT"; redhat; 
+elif [ -f /etc/debian_version ]; 
+	then echo "DEBIAN"; debian; 
+fi
 
 echo "Backing up original $osseccf file..."
 mv $osseccf $osseccf.orig.`date +"%m%d%y%T"`
